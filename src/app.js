eb378aa3d71ce7e1841bf86e0f21de5f1e7a261a
h4r0ldru1z <haroldand.ruiz@gmail.com>
2022-08-23 01:56:47 +0000

    cambios ultimos

--- src/app.js
+++ src/app.js
@@ -6,6 +6,33 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
 };
+function generateExcuse() {
+  let who = ["The dog", "My grandma", "His turtle", "My bird", "I"];
+  let action = ["ate", "peed", "crushed", "broke", "fall asleep over"];
+  let what = ["my homework", "the keys", "the car", "my lpatop"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying",
+    "at class"
+  ];
+  let whoIndx = Math.floor(Math.random() * who.length);
+  let actionIndx = Math.floor(Math.random() * action.length);
+  let whatIndx = Math.floor(Math.random() * what.length);
+  let whenIndx = Math.floor(Math.random() * when.length);
+  return (
+    who[whoIndx] +
+    " " +
+    action[actionIndx] +
+    " " +
+    what[whatIndx] +
+    " " +
+    when[whenIndx]
+  );
+}
